3ff15576bcee53aec88d2e0dee542c28bb056a98
Alexsouthwick <acsouthwick@gmail.com>
2016-08-01 14:43:54 -0700

    made drop zone for draggable boxes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
     $(".draggable").draggable();
+    $(".droppable").droppable({
+        drop: function(event, ui) {
+            $(this)
+            .addClass("highlighted")
+            .find("p")
+            .html("dropped");
+        }
+    });
 });
