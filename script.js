82cb000158beccc2b9ad097fd67e8833e148ea1d
Alexsouthwick <acsouthwick@gmail.com>
2016-08-02 11:47:24 -0700

    made it right

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 $(document).ready(function() {
+    alert("something should be happening1");
     $(".draggable").draggable();
     $(".droppable").droppable({
-        drop: function(event, ui) {
-            $(this)
-            .addClass("highlighted")
-            .find("p")
-            .html("dropped");
-        }
+         drop: function(event, ui) {
+             $(this)
+             .addClass("highlighted")
+             .find("p")
+             .html("dropped");
+             alert("something should be happening");
+             $(".highlighted").on("mouseup", add_list);
+         }
     });
-});
+    var check=false;
+    var width= $(".draggable").width();
+    alert(width);
+    $(".check").on("click", checked);
+    alert(check);
+    // alert(".draggable".css(width));
+ });
 
-var w = $('#block_list ul').width();
 
-$("#block_list >ul").animate({
-    left: -w
-}, 30000)
+ function checked() {
+    check=true;
+    alert(check);
+    if(check==true) {
+     $("#blocks_list").append("<li>width</li>");
+     $("#song").append("<li> song item </li>");
+    alert("added to list");
+    alert(blocks_list);
+    alert(song);
+ }
+ }
+
 
 
 $(document).ready(function(){
