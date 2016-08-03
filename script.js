a86cb95562a1e72771405abefbe3fbc4c7398c50
chloetanlimco <chloetlc@yahoo.com>
2016-08-03 13:53:18 -0700

    not done

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 
  var highlight=false;
  var combined_width= 0;
+ var song= "";
 $(document).ready(function() {
     alert("something should happen1")
     $(".draggable").draggable();
     $(".droppable").droppable({
          drop: function(event, ui) {
-             $(this)
+             $(this).append(ui.draggable.css({position: 'relative'}));
              .addClass("highlighted")
-             .find("p")
-             .html("dropped");
              highlight= true;
              alert(highlight);
              alert("something should be happening");
@@ -19,8 +18,33 @@ $(document).ready(function() {
                 alert($("#blocks_list li").length);
                 combined_width= ((width) * ($("#blocks_list li").length));
                 alert(combined_width);
-                $("#song").append("<li> song item </li>");
+                var draggableid= ui.draggable.find("h4").attr("id");
+                alert(draggableid);
+                string_draggableid= String(draggableid);
+                alert(string_draggableid);
+                // melody= document.getElementById("draggableid").innerHTML;
+                melody= ui.draggable.find("h4").attr("id").getElementsByTagName('h4')[0].innerHTML;
+                alert(melody);
+                string_melody= String(melody);
+                alert(string_melody);
+                // melody2= block2.getElementsByTagName('h4')[0].innerHTML;
+                // // alert(melody2);
+                //  string_melody2= String(melody2);
+                // alert(string_melody2);
 
+                if (string_melody.localeCompare("C G E B")){
+                 song += "cgeb";
+                 highlight= false; 
+                 alert(song);  
+                 }
+
+                 else if (string_melody.localeCompare("F G B A")){
+                 song += "fgba"; 
+                 alert(song);  
+                 }
+                 else
+                    {alert("else happened");}
+                
     }
          }
         
@@ -31,29 +55,6 @@ $(document).ready(function() {
      $("div.Scroller").scrollLeft(300);
  });
 
-// var combined_width= 0;
-
- // function checked() {
- //    var width= $(".draggable").width();
- //    check=true;
- //    alert( combined_width);
-    // if (highlight == true) {
-    //  $("#blocks_list").append("<li> block </li>");
-    //  alert($("#blocks_list li").length);
-    //  combined_width= ((width) * ($("#blocks_list li").length));
-    //  alert(combined_width);
-    //  $("#song").append("<li> song item </li>");
-
-    // }
-    // if(check==true) {
-    //  $("#blocks_list").append("<li> block </li>");
-    //  alert($("#blocks_list li").length);
-    //  combined_width= ((width) * ($("#blocks_list li").length));
-    //  alert(combined_width);
-    //  $("#song").append("<li> song item </li>");
-
- // }
- // }
 
 $(document).ready(function(){
     var Disney_clicked = "notclicked";
